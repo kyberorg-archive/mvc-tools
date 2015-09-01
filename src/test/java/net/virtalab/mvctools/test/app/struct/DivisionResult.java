d552c0f03b6f87824816c95eb24170dac1c0012d
Alex Muravya <asm@virtalab.net>
2015-09-01 13:15:07 +0300

    Making tests

---
+++ src/test/java/net/virtalab/mvctools/test/app/struct/DivisionResult.java
@@ -0,0 +1,11 @@
+package net.virtalab.mvctools.test.app.struct;
+
+/**
+ * Class description
+ *
+ * @author Alexander Muravya {@literal <muravya@corp.sputnik.ru>}
+ * @since 0.0
+ */
+public class DivisionResult {
+    public double result;
+}
---
+++ src/test/java/net/virtalab/mvctools/test/app/struct/DivisionStruct.java
@@ -0,0 +1,12 @@
+package net.virtalab.mvctools.test.app.struct;
+
+/**
+ * Structure for Division test webapp part
+ *
+ * @author Alexander Muravya {@literal <muravya@corp.sputnik.ru>}
+ * @since 2.2
+ */
+public class DivisionStruct {
+    public double numerator;
+    public double denominator;
+}
